24e173ab783535fbd9ee4cd62d1b4d941a54ff79
boris nikiforov <nikiforov5000@gmail.com>
2021-03-26 14:08:48 +0300

    ARCH: created app with devise, created Project model and controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,7 @@
+class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home, :index]
+  def index
+    @projects = Project.all
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_091911) do
+ActiveRecord::Schema.define(version: 2021_03_26_093005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "url"
+    t.string "github_link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
